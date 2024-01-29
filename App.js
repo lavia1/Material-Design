33f4e7d4fb2702597856ee5de9d0745407ae6dff
Laura <127945558+lavia1@users.noreply.github.com>
2024-01-29 17:06:45 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Login from './screens/Login'
+import { PaperProvider } from 'react-native-paper';
+
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <Login />
+    </PaperProvider>
+  );
+}
+
+
